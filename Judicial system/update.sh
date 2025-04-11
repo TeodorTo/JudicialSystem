9e9aeb78e11a3f70df960ef6cca72ffbb36f7b94
Teodor Todorov <todorov1302@gmail.com>
2025-04-11 16:32:48 +0300

    Updated update.sh

--- Judicial system/update.sh
+++ Judicial system/update.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
 cd /var/www/JudicialSystem/Judicial\ system/
+
+# Спри сервиза
+sudo systemctl stop judicialsystem.service
+
+# Обнови от git
 sudo git fetch origin
 sudo git reset --hard origin/master
+
+# Изтрий старата публикация
+sudo rm -rf /var/www/JudicialSystem/Judicial\ system/publish/
+
+# Публикувай проекта
 sudo /usr/bin/dotnet publish -c Release -o /var/www/JudicialSystem/Judicial\ system/publish/
+
+# Смени правата
 sudo chown -R www-data:www-data /var/www/JudicialSystem/Judicial\ system/publish/
-sudo systemctl restart judicialsystem.service
+
+# Стартирай отново сервиза
+sudo systemctl start judicialsystem.service
