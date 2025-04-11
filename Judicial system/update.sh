e564fa87bc14939ded52562d27a077c25f7e76da
Teodor Todorov <todorov1302@gmail.com>
2025-04-11 14:28:45 +0300

    updated update.sh

--- Judicial system/update.sh
+++ Judicial system/update.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 cd /var/www/JudicialSystem/Judicial\ system/
-git pull origin master
+sudo git fetch origin
+sudo git reset --hard origin/master
 sudo /usr/bin/dotnet publish -c Release -o /var/www/JudicialSystem/Judicial\ system/publish/
 sudo chown -R www-data:www-data /var/www/JudicialSystem/Judicial\ system/publish/
 sudo systemctl restart judicialsystem.service
